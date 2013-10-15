802c3dfbf5fbb3d3066d164420e55104aa4089ec
Zach Cox <zach.cox@banno.com>
2013-10-15 15:41:31 -0500

    blockInPoolFuture

--- README.md
+++ README.md
@@ -4,9 +4,11 @@ To run, simply do `re-start` in sbt.
 
 If you `curl http://localhost:8080/blockThisThread/10` and then try to `curl http://localhost:8080/ping` those ping requests will timeout, because the single service actor's runRoute function is blocked by Thread.sleep, those requests pile up in its mailbox, and then something lower-level in Spray times out.
 
-However the `/blockInFuture/10` and `/blockInDetach/10` routes will avoid this behavior because the Thread.sleep calls now (presumably) block some other thread and allow the service actor's runRoute function to continue processing incoming HTTP request messages. Note though, that these other Futures/Actors are still running on the default Akka dispatcher, as is the service actor, and are blocking its threads. So eventually if enough requests for those routes come in concurrently, you probably will run out of threads and the service actor will be unable to continue processing incoming request messages.
+The `/blockInFuture/10` and `/blockInDetach/10` routes will (mostly) avoid this behavior because the Thread.sleep calls now block some other thread and allow the service actor's runRoute function to continue processing incoming HTTP request messages. Note though, that these other Futures/Actors are still running on the default Akka dispatcher, as is the service actor, and are blocking its threads. So eventually if enough requests for those routes come in concurrently, you will run out of threads and the service actor will be unable to continue processing incoming request messages. You can force this behavior by running many `curl http://localhost:8080/blockInFuture/10 &` calls concurrently. The number of calls depends on the number of threads in the default dispatcher's pool.
+
+To ensure that the service actor can continue handling incoming http request messages, the `/blockInPoolFuture/10` route does the blocking off in a separate Future under a separate execution context, not in the default Akka dispatcher. So you can run as many `curl http://localhost:8080/blockInPoolFuture/10 &` requests as you want, and `/ping` will continue to respond immediately.
 
 So some best practices probably are:
-  * don't call functions that will take time to return directly in routes
-  * do call functions that will take time to return separately in a Future or detachTo or some other Actor
+  * don't call long-running functions directly in routes
+  * do call long-running functions separately in a Future or detachTo or some other Actor
   * think about using some other ExecutionContext/Dispatcher for those long-running functions
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,16 +1,13 @@
 package blocking
 
-import spray.routing.SimpleRoutingApp
+import spray.routing.{SimpleRoutingApp, Route}
 import akka.actor.ActorSystem
-import scala.concurrent.Future
+import scala.concurrent._
+import java.util.concurrent.Executors
 
 object Main extends App with SimpleRoutingApp {
   implicit val system = ActorSystem("my-system")
   import system.dispatcher
-
-  startServer(interface = "localhost", port = 8080) {
-    get(pingPong ~ blockThisThread ~ blockInFuture ~ blockInDetach)
-  }
 
   def block(delay: Long) = {
     println(s"Blocking for $delay secs...")
@@ -19,8 +16,28 @@ object Main extends App with SimpleRoutingApp {
     "Done blocking"
   }
 
-  def blockThisThread = path("blockThisThread" / IntNumber)(delay => complete(block(delay)))
-  def blockInFuture = path("blockInFuture" / IntNumber)(delay => complete(Future(block(delay))))
-  def blockInDetach = path("blockInDetach" / IntNumber)(delay => detachTo(singleRequestServiceActor)(complete(block(delay))))
-  def pingPong = path("ping")(complete("pong"))
+  /** Simple route to test responsiveness of service. */
+  val pingPong = path("ping")(complete("pong"))
+
+  /** Blocks right on the calling thread. */
+  val blockThisThread = path("blockThisThread" / IntNumber)(delay => complete(block(delay)))
+
+  /** Blocks off in a Future, not on calling thread. But note that this Future uses the ActorSystem's default message dispatcher, which the 
+    * spray-routing service actor is also using, so it is possible to block all of that dispatcher's threads and prevent spray-routing from 
+    * processing new http request messages. */
+  val blockInFuture = path("blockInFuture" / IntNumber)(delay => complete(future(block(delay))))
+
+  val context = ExecutionContext.fromExecutorService(Executors.newFixedThreadPool(5))
+  def poolFuture[T](body: => T): Future[T] = future(body)(context)
+  /** Blocks off in a Future that uses its own execution context, not the ActorSystem's default dispatcher. Thus you can hit this to your heart's 
+    * content and it will not prevent the spray-routing service actor from handling incoming http request messages. */
+  val blockInPoolFuture = path("blockInPoolFuture" / IntNumber)(delay => complete(poolFuture(block(delay))))
+
+  def detach(route: Route) = detachTo(singleRequestServiceActor)(route)
+  /** Spawns a new Actor on each request and blocks on that actor. Same note about message dispatcher applies as for blockInFuture. */
+  val blockInDetach = path("blockInDetach" / IntNumber)(delay => detach(complete(block(delay))))
+
+  startServer(interface = "localhost", port = 8080) {
+    get(pingPong ~ blockThisThread ~ blockInFuture ~ blockInPoolFuture ~ blockInDetach)
+  }
 }
